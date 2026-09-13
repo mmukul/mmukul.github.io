f4e4a7e99790e356824092964e57038fa757ffca
NextGen DevSecOps & GenAI <smilemukul2005@gmail.com>
2026-09-13 15:40:02 +0530

    Delete admin-portal directory

--- admin-portal/js/supabase-config.js
+++
@@ -1,4 +0,0 @@
-/* Add your Supabase project values before deployment. Never use the service_role key here. */
-window.NEXTGEN_SUPABASE_URL = '';
-window.NEXTGEN_SUPABASE_ANON_KEY = '';
-window.NEXTGEN_SUPABASE_CONFIGURED = Boolean(window.NEXTGEN_SUPABASE_URL && window.NEXTGEN_SUPABASE_ANON_KEY);
