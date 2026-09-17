9c3752f0d9d7c67badfc584af253638a725c16d9
NextGen DevSecOps & GenAI <smilemukul2005@gmail.com>
2026-09-17 14:13:45 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+window.NEXTGEN_SUPABASE_URL = window.NEXTGEN_SUPABASE_URL || 'https://hkpvigvtdckxhmnsvdrh.supabase.co';
+window.NEXTGEN_SUPABASE_ANON_KEY = window.NEXTGEN_SUPABASE_ANON_KEY || 'sb_publishable_JlIrNdsEXgjikrh9F_YxYg_DBrgimoQ';
+window.NEXTGEN_SUPABASE_CONFIGURED = Boolean(window.NEXTGEN_SUPABASE_URL && window.NEXTGEN_SUPABASE_ANON_KEY);
