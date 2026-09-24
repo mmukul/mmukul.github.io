c7eeacea0b83c8c59fe7edd4a1cdfd85d2cea71a
NextGen DevSecOps AI <smilemukul2005@gmail.com>
2026-09-24 12:56:34 +0530

    Delete admin-portal/js directory

--- admin-portal/js/supabase-config.js
+++
@@ -1,3 +0,0 @@
-window.NEXTGEN_SUPABASE_URL = window.NEXTGEN_SUPABASE_URL || 'https://hkpvigvtdckxhmnsvdrh.supabase.co';
-window.NEXTGEN_SUPABASE_ANON_KEY = window.NEXTGEN_SUPABASE_ANON_KEY || 'sb_publishable_JlIrNdsEXgjikrh9F_YxYg_DBrgimoQ';
-window.NEXTGEN_SUPABASE_CONFIGURED = Boolean(window.NEXTGEN_SUPABASE_URL && window.NEXTGEN_SUPABASE_ANON_KEY);
